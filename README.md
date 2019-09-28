ea013f1185ff48d735d63c3f9bdb56f52a18e939
Pankti <mishthii95@hotmail.com>
2019-09-28 14:29:41 +0530

    set up the response data and hosted json on a local server

---
+++ README.md
@@ -0,0 +1,3 @@
+run the following commands in cmd prompt
+npm install -g json-server
+then run json-server --watch data/hotelresponse.json
--- js/app.js
+++ js/app.js
@@ -1,23 +1,23 @@
 
 
-// $(document).ready(function() {
+$(document).ready(function() {
 //
-// 	$.ajax({
-// 		url: 'http://localhost:3000/hotel-search-response',
-// 		dataType: 'jsonp',
-// 		success:  function (res) {
-// 			let o = res.hotels;
+	$.ajax({
+		url: 'http://localhost:3000/hotel-search-response',
+		dataType: 'jsonp',
+		success:  function (res) {
+			let o = res.hotels;
 // 			// console.log(o["basic-info"]["hotel-info:hotel-name"]);
-// 			// console.log(o);
-// 			console.log(o.hotel);
+			// console.log(o);
+			console.log(o.hotel);
 // 			// console.log(o.hotel[0]["room-rates"]["room-rate"]);
 // 			let roomRateArr = o.hotel[0]["room-rates"]["room-rate"]; // to get common:pricing-element
 // 			// console.log(roomRateArr[0]["rate-breakdown"]["common:rate"]["common:pricing-elements"]);
 // 			let commonPricing = roomRateArr[0]["rate-breakdown"]["common:rate"]["common:pricing-elements"];
 // 			// console.log(commonPricing);
 // 			let thumbnailimg = o.hotel[0]["basic-info"]["thumb-nail-image"];
-// 		}
+		}
+
+	});
 //
-// 	});
-//
-// 	})
+	})
